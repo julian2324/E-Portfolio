bb8153757e051ddc2ec227309bf3294637d4027d
Julian <julian.gold.crenshaw@gmail.com>
2025-08-14 14:41:41 -0500

    initialize git repository

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger =
+document.getElementById('hamburger');
+const navMenu =
+document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+}); 
+
